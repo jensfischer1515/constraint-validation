79c67a612a97d178aed1fe35a8078071b9b13139
Jens Fischer <soth@gmx.net>
2017-10-16 15:20:44 +0200

    initial import

---
+++ src/main/java/jensfischerhh/constraintvalidation/ConstraintValidationApplication.java
@@ -0,0 +1,12 @@
+package jensfischerhh.constraintvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstraintValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstraintValidationApplication.class, args);
+	}
+}
